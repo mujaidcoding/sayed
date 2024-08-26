a441142dd685b4625a8baef466c78f09e45f3551
Mujaid <mujaidcoding@gmail.com>
2024-08-27 01:24:31 +0600

    initial commit

---
+++ resources/views/frontend/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+   </head>
+
+   <body>
+
+      <button
+         class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+         Button
+      </button>
+   </body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+      @vite('resources/css/app.css')
+   </head>
+
+   <body>
+
+      <button
+         class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+         Button
+      </button>
+
+      @vite('resources/js/app.js')
+   </body>
+
+</html>
