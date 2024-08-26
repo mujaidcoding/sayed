a8201d24a6d86262fe39585d9ca8ec0dce401629
Mujaid <mujaidcoding@gmail.com>
2024-08-27 02:15:41 +0600

    test live

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Define the path to the Laravel public directory
+define('LARAVEL_PUBLIC_PATH', __DIR__.'/../public');
+
+// Include the real Laravel index.php
+require LARAVEL_PUBLIC_PATH.'/index.php';
