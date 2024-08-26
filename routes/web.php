fa9ef692d224d04b66234625a5907acaa685d69d
Mujaid <mujaidcoding@gmail.com>
2024-08-27 02:36:37 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clear-all-cache', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+
+    return response()->json(['message' => 'All caches cleared successfully.']);
 });
 
 Route::get('/dashboard', function () {
